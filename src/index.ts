59b1d8c42ce67588755acd611c23b25518d82ed0
Zach Robinson <zprobinson@sio.midco.net>
2021-11-18 22:06:21 -0600

    exported Button component from main index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { default as Columns, ColumnsProps } from "./Columns";
 export { default as Column, ColumnProps } from "./Column";
 export { default as Block, BlockProps } from "./Block";
 export { default as Box, BoxProps } from "./Box";
+export { default as Button, ButtonProps } from "./Button";
 
 export * from "./types";
