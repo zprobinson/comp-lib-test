841fdf783ee8377eccb339bbda2a3e1800c965da
Zach Robinson <zprobinson@sio.midco.net>
2021-11-18 21:05:25 -0600

    generated Button component

---
+++ src/Button/Button.tsx
@@ -0,0 +1,12 @@
+// Generated with util/create-component.js
+import React from "react";
+
+import { ButtonProps } from "./Button.types";
+
+const Button: React.FC<ButtonProps> = ({ foo }) => (
+  <div data-testid="Button" className="foo-bar">
+    {foo}
+  </div>
+);
+
+export default Button;
