1493428627f185e8278bfb795dc2661be32a8b18
Zach Robinson <zprobinson@sio.midco.net>
2021-11-20 23:44:10 -0600

    exported Content and Delete from lib

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export { default as Column, ColumnProps } from "./Column";
 export { default as Block, BlockProps } from "./Block";
 export { default as Box, BoxProps } from "./Box";
 export { default as Button, ButtonProps } from "./Button";
+export { default as Content, ContentProps } from "./Content";
+export { default as Delete, DeleteProps } from "./Delete";
 
 export * from "./types";
