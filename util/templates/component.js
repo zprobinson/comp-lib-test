224d0f13e163823738e46c92b5cf58bb7c6d70ee
Zach Robinson <zprobinson@sio.midco.net>
2022-01-04 00:46:32 -0600

    test updating generation scripts

--- util/templates/component.js
+++ util/templates/component.js
@@ -1,17 +1,19 @@
 module.exports = (componentName) => ({
-  content: `// Generated with util/create-component.js
+  content: `
 import React from "react";
 
 import { ${componentName}Props } from "./${componentName}.types";
 
 import "./${componentName}.scss";
 
-const ${componentName}: React.FC<${componentName}Props> = ({ foo }) => (
+const ${componentName}: React.FC<${componentName}Props> = ({ foo }) => {
+  return (
     <div data-testid="${componentName}" className="foo-bar">{foo}</div>
-);
+  )
+};
 
 export default ${componentName};
 
 `,
-  extension: `.tsx`
+  extension: `.tsx`,
 });
