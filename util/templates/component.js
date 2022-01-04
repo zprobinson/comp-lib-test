a231522fed0b705b3a671891140d3a052fef34ef
Zach Robinson <zprobinson@sio.midco.net>
2022-01-04 00:48:30 -0600

    removed scss reference from generated component file

--- util/templates/component.js
+++ util/templates/component.js
@@ -4,8 +4,6 @@ import React from "react";
 
 import { ${componentName}Props } from "./${componentName}.types";
 
-import "./${componentName}.scss";
-
 const ${componentName}: React.FC<${componentName}Props> = ({ foo }) => {
   return (
     <div data-testid="${componentName}" className="foo-bar">{foo}</div>
