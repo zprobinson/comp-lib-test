bd7f76243f4e253fca8c45b5082ff51d4c583701
Zach Robinson <zprobinson@sio.midco.net>
2022-01-05 23:17:44 -0600

    added TODO in withBulmaProps for not passing in { className?: string }

--- src/bulma.tsx
+++ src/bulma.tsx
@@ -3,6 +3,9 @@ import { BulmaHelpers } from "./types";
 import { partitionBulmaPropsG } from "./utilities/propUtilities";
 import { foldClassNames, foldHelpers } from "./utilities/listUtils";
 
+// TODO: This only works if the incoming TProps explicitly includes the { className?: string }
+// We should be able to pass in TProps where it doesn't include a className. -z
+
 const withBulmaProps = <TProps extends {} = {}>(
   WrappedComponent: React.ComponentType<TProps>
 ) => {
