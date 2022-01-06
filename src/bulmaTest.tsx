9111d8156ab38573bbe1d38795c1ea8c2b1067f1
Zach Robinson <zprobinson@sio.midco.net>
2022-01-05 23:34:51 -0600

    HOC bulmaTest

---
+++ src/bulmaTest.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BulmaHelpers } from "./types";
+import { partitionBulmaPropsG } from "./utilities/propUtilities";
+import { foldClassNames, foldHelpers } from "./utilities/listUtils";
+
+type Test<TProps extends {} = {}> = TProps &
+  BulmaHelpers &
+  ({ className?: string } | {});
+
+const withBulmaPropsTest = <TProps extends {} = {}>(
+  WrappedComponent: React.ComponentType<TProps>
+) => {
+  const ComponentWithBulmaProps = (props: Test<TProps>) => {
+    const { bulmaProps, componentProps } = partitionBulmaPropsG<TProps>(props);
+    const helpers = foldHelpers(bulmaProps);
+    const classNames =
+      "className" in props
+        ? foldClassNames([props.className ?? "", helpers])
+        : helpers;
+
+    return (
+      <WrappedComponent
+        {...(componentProps as TProps)}
+        className={classNames}
+      />
+    );
+  };
+
+  return ComponentWithBulmaProps;
+};
+
+export default withBulmaPropsTest;
