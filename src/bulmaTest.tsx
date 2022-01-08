e4a1206bf277d0cdfe2eb11e9bd8067d963dff15
Zach Robinson <zprobinson@sio.midco.net>
2022-01-07 21:32:04 -0600

    changed generics on return HOC

---
+++ src/bulmaTest.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BulmaHelpers } from ".";
+import { foldClassNames, foldHelpers } from "./utilities/listUtils";
+import { partitionBulmaPropsG } from "./utilities/propUtilities";
+
+const withBulmaPropsTest = <
+  T extends { className?: string } = { className?: string }
+>(
+  WrappedComponent: React.ComponentType<T>
+) => {
+  const displayName =
+    WrappedComponent.displayName ?? WrappedComponent.name ?? "Component";
+
+  const ComponentWithBulmaProps = <TInner extends T & BulmaHelpers>(
+    props: TInner
+  ) => {
+    const { bulmaProps, componentProps } = partitionBulmaPropsG<TInner>(props);
+    const helpers = foldHelpers(bulmaProps);
+    const classNames = foldClassNames([props.className ?? "", helpers]);
+
+    return (
+      <WrappedComponent
+        {...(componentProps as TInner)}
+        className={classNames}
+      />
+    );
+  };
+
+  ComponentWithBulmaProps.name = displayName;
+
+  return ComponentWithBulmaProps;
+};
+
+export default withBulmaPropsTest;
