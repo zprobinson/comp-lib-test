1ed3d664f34e340dc91565bf013c78758b076cd8
Zach Robinson <zprobinson@sio.midco.net>
2022-01-07 21:25:54 -0600

    additional withBulmaPropsTest test implementation

--- src/bulma.tsx
+++ src/bulma.tsx
@@ -6,7 +6,9 @@ import { foldClassNames, foldHelpers } from "./utilities/listUtils";
 const withBulmaProps = <TProps extends { className?: string } = {}>(
   WrappedComponent: React.ComponentType<TProps>
 ) => {
-  const ComponentWithBulmaProps = (props: TProps & BulmaHelpers) => {
+  const ComponentWithBulmaProps = (
+    props: React.PropsWithChildren<TProps> & BulmaHelpers
+  ) => {
     const { bulmaProps, componentProps } = partitionBulmaPropsG<TProps>(props);
     const helpers = foldHelpers(bulmaProps);
     const classNames = foldClassNames([props.className ?? "", helpers]);
