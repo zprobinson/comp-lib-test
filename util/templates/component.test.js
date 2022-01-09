faeeb89662cbe6aaf00461a3c87e8f0fd3b8ca62
Zach Robinson <zprobinson@sio.midco.net>
2022-01-08 21:40:19 -0600

    improved test file generate util

--- util/templates/component.test.js
+++ util/templates/component.test.js
@@ -6,24 +6,16 @@ import { render } from "@testing-library/react";
 import ${componentName} from "./${componentName}";
 import { ${componentName}Props } from "./${componentName}.types";
 
-describe("Test Component", () => {
-  let props: ${componentName}Props;
+describe("${componentName} Component", () => {
+  const renderComponent = (props: ${componentName}Props) => render(<${componentName} {...props} />);
 
-  beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
-
-  const renderComponent = () => render(<${componentName} {...props} />);
-
-  it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    const { getByTestId } = renderComponent();
+  it("should render children correctly", () => {
+    const expected = "harvey was here";
+    const { getByTestId } = renderComponent({ children: expected });
 
     const component = getByTestId("${componentName}");
 
-    expect(component).toHaveTextContent("harvey was here");
+    expect(component).toHaveTextContent(expected);
   });
 });
 `,
