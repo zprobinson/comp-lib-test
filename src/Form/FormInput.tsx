96cfba37a97736d3f385b339e5cb7444ca4953f8
Zach Robinson <zprobinson@sio.midco.net>
2022-01-12 23:52:12 -0600

    form input forwardRef test

--- src/Form/FormInput.tsx
+++ src/Form/FormInput.tsx
@@ -5,36 +5,47 @@ import useFormFieldContext from "./formFieldContext";
 
 import { InnerFormInputProps } from "./Form.types";
 
-const FormInput: React.FC<InnerFormInputProps> = ({
-  className,
-  size,
-  color,
-  state,
-  isStatic = false,
-  isLoading = false,
-  isRounded = false,
-  type,
-  ...props
-}) => {
-  const context = useFormFieldContext();
-  const classNames = foldClassNames([
-    className ?? "",
-    size ?? context.size ?? "",
-    color ?? "",
-    state ?? "",
-    isStatic ? "is-static" : "",
-    isLoading ? "is-loading" : "",
-    isRounded ? "is-rounded" : "",
-  ]);
+const FormInput: React.FC<InnerFormInputProps> = React.forwardRef<
+  HTMLInputElement,
+  InnerFormInputProps
+>(
+  (
+    {
+      className,
+      size,
+      color,
+      state,
+      isStatic = false,
+      isLoading = false,
+      isRounded = false,
+      type,
+      ...props
+    },
+    ref
+  ) => {
+    const context = useFormFieldContext();
+    const classNames = foldClassNames([
+      className ?? "",
+      size ?? context.size ?? "",
+      color ?? "",
+      state ?? "",
+      isStatic ? "is-static" : "",
+      isLoading ? "is-loading" : "",
+      isRounded ? "is-rounded" : "",
+    ]);
 
-  return (
-    <input
-      data-testid="FormInput"
-      type={type ?? "text"}
-      className={`input ${classNames}`}
-      {...props}
-    ></input>
-  );
-};
+    return (
+      <input
+        data-testid="FormInput"
+        type={type ?? "text"}
+        className={`input ${classNames}`}
+        ref={ref}
+        {...props}
+      ></input>
+    );
+  }
+);
+
+export const FormInputWithoutBulmaProps = FormInput;
 
 export default withBulmaProps(FormInput);
